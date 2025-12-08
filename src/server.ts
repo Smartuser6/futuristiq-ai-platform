e440eb9825a2081f3cd721f6a6df8526b8a82dfb
Smartuser6 <delimasantoswarlley@gmail.com>
2025-12-08 11:53:30 +0100

    Delete src directory

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-// src/server.ts
-// Koppelt den Deno-Deploy-Fetch mit unserem Router
-
-import { router } from "./routes/mod.ts";
-
-export async function handleRequest(request: Request): Promise<Response> {
-  return router.handle(request);
-}
