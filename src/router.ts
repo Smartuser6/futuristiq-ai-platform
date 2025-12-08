7d138e438a99fd826efb6bff10f27219885cb5ba
Smartuser6 <delimasantoswarlley@gmail.com>
2025-12-08 12:26:24 +0100

    Implement Router class for handling routes

---
+++ src/router.ts
@@ -0,0 +1,27 @@
+type Handler = (req: Request) => Promise<Response> | Response;
+
+interface Route {
+  method: string;
+  path: string;
+  handler: Handler;
+}
+
+class Router {
+  routes: Route[] = [];
+
+  add(method: string, path: string, handler: Handler) {
+    this.routes.push({ method, path, handler });
+  }
+
+  match(path: string, method: string) {
+    return this.routes.find(r => r.method === method && r.path === path);
+  }
+}
+
+const router = new Router();
+
+// Import all route modules
+import "./routes/mod.ts";
+
+export default router;
+export { router };
